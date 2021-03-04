13a3bf7116ac99377350aeba7db524e723d3280f
Marieta28 <69346003+Marieta28@users.noreply.github.com>
2021-03-04 16:27:27 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+ $(window).scroll(function() {
+        $('.skill-img').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
+ $(window).scroll(function() {
+        $('.skill-title').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
+ $(window).scroll(function() {
+        $('.skill-text').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
+$(window).scroll(function() {
+        $('.section-title').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
+$(window).scroll(function() {
+        $('.btn').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
+ $(window).scroll(function() {
+        $('.form-input').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
+
+ $(window).scroll(function() {
+        $('.footer-text').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }       
+         });
+});
